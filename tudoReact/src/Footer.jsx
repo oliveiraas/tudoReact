1f354275c40a10c27cad5ef84485f101a65fb705
Adriana Oliveira <78982435+oliveiraas@users.noreply.github.com>
2025-02-19 19:54:29 -0300

    Add files via upload

---
+++ tudoReact/src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App'; // Importe o arquivo de estilos
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <br></br>
+      <p>Desenvolvido pela <strong> turma de Programação Web </strong>#TECHINCLUSAO | 2025</p>
+      
+    </footer>
+  );
+}
+
+export default Footer;
